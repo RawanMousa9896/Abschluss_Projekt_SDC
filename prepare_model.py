25c83f5565790d94e8817b66f9c2016672f45639
Rawan Mousa <91124664+RawanMousa9896@users.noreply.github.com>
2024-11-14 10:26:05 +0000

    installing

--- prepare_model.py
+++ prepare_model.py
@@ -5,3 +5,4 @@ model = tf.keras.applications.MobileNetV2(weights="imagenet")
 
 # Speichern des Modells im SavedModel-Format
 tf.saved_model.save(model, "models/plant_classifier/1")  # Speichert die erste Version des Modells im SavedModel-Format
+
